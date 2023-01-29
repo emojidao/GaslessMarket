d643adf9e6a161129ee0d73fd48a8cdf84b3271c
薛召 <xuezhao111@126.com>
2023-01-29 11:08:35 +0800

    test DCL

---
+++ contracts/0_dcl/MetadataCheckerEST.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+import "../validater/IMetadataChecker.sol";
+
+contract MetadataCheckerEST is IMetadataChecker {
+    function check(address nft721, uint256 nftId, bytes32 metadataHash) public view returns (bool) {
+        bytes memory data = abi.encodeWithSignature("getFingerprint(uint256)", nftId);
+        (bool success, bytes memory returndata) = nft721.staticcall(data);
+        return success && metadataHash == keccak256(returndata);
+    }
+}
